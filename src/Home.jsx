b6ca50303fd98ece22b131cefd21937390e86bb1
José Maldonado <joan.maldonadoa@gmail.com>
2022-08-20 21:18:52 -0400

    - Added favicon - Added css file - Added navbar icon - Created Home component - Created Navbar component - Changed Navbar style

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "./ui/components/Navbar";
+
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>DND</h1>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Home } from "./Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/Navbar.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
